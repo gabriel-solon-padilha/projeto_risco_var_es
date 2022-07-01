dc15ccf6050de28639f6b334f7706d96256688dc
Gabriel Solon Padilha <66848135+gabriel-solon-padilha@users.noreply.github.com>
2022-06-30 21:13:45 -0300

    feat: função es e função teste

--- functions.py
+++ functions.py
@@ -36,7 +36,7 @@ def calcula_log_retorno(pd_series,hp,descending):
         return 'Não foi inserido uma pandas series'
     elif not isinstance(pd_series.values, int) and not not isinstance(pd_series.values, float):
         return 'Foi inserido uma pandas series que não é inteira ou float'  
-    elif not isinstance(str(hp),int):
+    elif not isinstance(hp,str) and not isinstance(hp,int):
         return 'Foi inserido um valor de hp que não é um inteiro'
     elif not isinstance(descending,bool):
         return 'Foi inserido um valor de descending que não é boleano'
@@ -51,6 +51,11 @@ def calcula_log_retorno(pd_series,hp,descending):
         return df
 
 def calcula_var(pandas_serie_retornos, alpha, vlr_carteira_atual,descending):
+    '''
+    Recebe uma panda series de retornos, um alpha, um vlr da carteira atual e se é ascendente ou não
+
+    Retorna a métrica de risco var
+    '''
     if not isinstance(descending,bool):
         return 'Foi inserido um valor de descending que não é boleano'
     elif not isinstance(pandas_serie_retornos, pd.Series):
@@ -65,6 +70,8 @@ def calcula_var(pandas_serie_retornos, alpha, vlr_carteira_atual,descending):
         if descending:
             df.sort_index(inplace=True,ascending=False)
         df = df.to_frame()
+        #preço_auxiliar = p(t) - p(t-hp)
+        #p(t) - p(t-1) = retorno * p(t- hp)
         df['preco_auxiliar'] = np.ones(pandas_serie_retornos.shape[0])
         df.loc[0,'preco_auxiliar'] = vlr_carteira_atual
         #final_idx_preco_auxiliar = df.shape[0] - 1
@@ -74,9 +81,8 @@ def calcula_var(pandas_serie_retornos, alpha, vlr_carteira_atual,descending):
         #df.loc[1:,'preco_auxiliar'] = df.iloc[0:final_idx_preco_auxiliar,0]* df.loc[1:,'preco_auxiliar'] +  df.loc[1:,'preco_auxiliar']
         df['pnl'] = df.iloc[:,0] * df.loc[:,'preco_auxiliar']
         
+        
         var_quantile = df['pnl'].quantile(alpha)
-
-        df['pnl'].to_csv('teste.csv')
 
         mu, sigma = st.norm.fit(df['pnl'])
         z = st.norm.ppf(alpha)
@@ -84,11 +90,36 @@ def calcula_var(pandas_serie_retornos, alpha, vlr_carteira_atual,descending):
 
         return var_quantile, var_parametrico
 
+def calcula_es(pandas_serie_retornos, var,vlr_carteira_atual,descending):
+    '''
+    Recebe uma panda series de retornos, um var, um vlr da carteira atual e se é ascendente ou não
 
+    Retorna a métrica de risco var
+    '''
+    if not isinstance(descending,bool):
+        return 'Foi inserido um valor de descending que não é boleano'
+    elif not isinstance(pandas_serie_retornos, pd.Series):
+        return 'Não foi inserido uma pandas series'
+    elif not isinstance(var, int) and not isinstance(var, float):
+        return 'Valor do var não é inteiro e nem float'
+    elif not isinstance(vlr_carteira_atual, int) and not isinstance(vlr_carteira_atual, float):
+        return 'Valor carteira atual não é inteiro e nem float'
+    else:
+        pandas_serie_retornos.dropna(inplace=True)
+        df = pandas_serie_retornos.reset_index(drop=True)
+        if descending:
+            df.sort_index(inplace=True,ascending=False)
+        df = df.to_frame()
+        
+        df['preco_auxiliar'] = np.ones(pandas_serie_retornos.shape[0])
+        df.loc[0,'preco_auxiliar'] = vlr_carteira_atual
+        #final_idx_preco_auxiliar = df.shape[0] - 1
+        for i in range(1,pandas_serie_retornos.shape[0]):
+            df.loc[i,'preco_auxiliar'] = df.iloc[(i-1),0]* df.loc[(i-1),'preco_auxiliar'] + df.loc[(i-1),'preco_auxiliar'] 
+        #TODO corrigir essa lina abaixo para não precisar do loop (por algum motivo que ainda não entendi está dando pau)
+        #df.loc[1:,'preco_auxiliar'] = df.iloc[0:final_idx_preco_auxiliar,0]* df.loc[1:,'preco_auxiliar'] +  df.loc[1:,'preco_auxiliar']
+        df['pnl'] = df.iloc[:,0] * df.loc[:,'preco_auxiliar']
+        
+        es = df.loc[df['pnl'] <= var,'pnl'].mean()
 
-df = load_df()
-df_novo = calcula_retorno(df.price,1,False)
-print(df_novo)
-#df_novo = calcula_log_retorno(df_novo.price,1,False)
-var_quantile, var_parametrico = calcula_var(pandas_serie_retornos=df_novo['ret_rel_simples_1'],alpha=0.05,vlr_carteira_atual=5.6,descending=False)
-
+        return es
